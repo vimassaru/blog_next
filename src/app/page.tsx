dba89a2799dbc632c3a56281587aea65165669cd
Vinicius Tanaka <vinicius.tanaka@proaero.aero>
2025-05-21 12:11:04 -0300

    feature: adicionando imagens publicas e seed de dados para testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import clsx from "clsx";
+import { Header } from '@/components/Header';
 
 export default function HomePage() {
   return (
-    <h1
-      className={clsx(
-        "text-blue-500",
-        "text-7xl",
-        "hover:text-blue-50",
-        "hover:bg-blue-500",
-        "transition",
-        "duration-300"
-      )}
-    >
-      Hello World! HOMEPAGE
-    </h1>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,4 @@
+'use client';
+export function Header() {
+  return <h1>Header</h1>;
+}

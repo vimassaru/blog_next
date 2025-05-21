09b4eb731ead1a4d70f7bfb0680cd0ad18140501
Vinicius Tanaka <vinicius.tanaka@proaero.aero>
2025-05-21 11:26:34 -0300

    feature: adicionando .vscode, clsx para classes tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import clsx from "clsx";
+
 export default function HomePage() {
-  return <h1>Hello World! From HomePage</h1>;
+  return (
+    <h1
+      className={clsx(
+        "text-blue-500",
+        "text-7xl",
+        "hover:text-blue-50",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300"
+      )}
+    >
+      Hello World! HOMEPAGE
+    </h1>
+  );
 }

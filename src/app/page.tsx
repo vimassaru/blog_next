2bbdbaf3528d22e24e394801da0a52a3659be90c
Vinícius Tanaka <vi_massaru.tanaka@hotmail.com>
2025-05-22 13:35:35 -0300

    feature: adicionando estrutura section 1/2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { ThemeButton } from '@/components/ThemeButton';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -13,6 +15,24 @@ export default async function HomePage() {
       <div className='flex items-end justify-center'>
         <ThemeButton />
       </div>
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do Post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          consectetur assumenda unde sit perspiciatis ratione fugiat,
+          voluptatibus dolor necessitatibus ab laborum veritatis laboriosam sed
+          porro obcaecati iure deleniti iusto? Odio.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
